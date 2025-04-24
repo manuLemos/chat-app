c1a68e246ba52e0a783e90ba3c3be93d2b4408ed
Manuely Meireles Lemos <manumeireleslemos@gmail.com>
2025-04-23 21:46:00 -0300

    método onMessageReceived

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -42,6 +42,43 @@ function onConnected() {
     connectingElement.classList.add('hidden');
 }
 
+function onMessageReceived(payload){
+    var message = JSON.parse(payload.body);
+
+    var messageElement = document.createElement('li');
+
+    if(message.type === 'JOIN') {
+        messageElement.classList.add('event-message');
+        message.content = message.sender + ' joined!';
+    } else if (message.type === 'LEAVER') {
+        messageElement.classList.add('event-message');
+        message.content = message.sender + ' left!';
+    } else {
+        messageElement.classList.add('chat-message');
+
+        var avatarElement = document.createElement('i');
+        var avatarText = document.createTextNode(message.sender[0]);
+        avatarElement.appendChild(avatarText);
+        avatarElement.style['background-color'] = getAvatarColor(message.sender);
+
+        messageElement.appendChild(avatarElement);
+
+        var usernameElement = document.createElement('span');
+        var usernameText = document.createTextNode(message.sender);
+        usernameElement.appendChild(usernameText);
+        messageElement.appendChild(usernameElement);
+    }
+
+    var textElement = document.createElement('p');
+    var messageText = document.createTextNode(message.content);
+    textElement.appendChild(messageText);
+
+    messageElement.appendChild(textElement);
+
+    messageArea.appendChild(messageElement);
+    messageArea.scrollTop = messageArea.scrollHeight;
+}
+
 function sendMessage(event) {
 
     var messageContent = messageInput.value.trim();
