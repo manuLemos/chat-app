30c01e237d5965d23e8a56c1d9e3644501159919
Manuely Meireles Lemos <manumeireleslemos@gmail.com>
2025-04-23 21:43:00 -0300

    metodo connect

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -14,5 +14,19 @@ var colors = [
     '#2196F3', '#32c787', '#00BCD4', '#ff5652',
     '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
 ];
+
+function connect(event){
+    username = document.querySelector('#name').value.trim();
+    if (username){
+        usernamePage.classList.add('hidden');
+        chatPage.classList.remove('hidden');
+
+        var socket = new SockJS('/ws');
+        stompClient = Stomp.over(socket);
+
+        stompClient.connect({}, onConnected, onError);
+    }
+    event.preventDefault();
+}
 usernameForm.addEventListener('submit', connect, true);
 messageForm.addEventListener('submit', sendMessage, true);
